e834d906ad29a2fbfd17e524f29121060455bb75
sashapoliti <sas.politi@gmail.com>
2024-05-07 18:07:36 +0200

    more style, add footer, end

---
+++ Views/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container text-center ">
+        <span>made with <i class="fas fa-heart"></i> by Sasha</span>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -3,5 +3,6 @@ include __DIR__ . "/Views/topCode.php";
 
 include __DIR__ ."/Views/header.php";
 include __DIR__ ."/Views/main.php";
+include __DIR__ ."/Views/footer.php";
 
 include __DIR__ . "/Views/bottomCode.php";
